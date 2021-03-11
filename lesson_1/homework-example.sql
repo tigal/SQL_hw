c8e9248717b0b469533f8637dd93001a9b202651
Galina <45859134+tigal@users.noreply.github.com>
2021-03-11 16:38:05 +0300

    Create homework-example.sql

---
+++ lesson_1/homework-example.sql
@@ -0,0 +1,19 @@
+/* Практическое задание #0. */
+
+/* Задание # 1.
+ * Описание задания (скопировать задание из файла с дз)
+*/
+-- создание базы данных университета
+create database university;
+-- создание таблицы student
+create table student (name VARCHAR(100), surname VARCHAR(100));
+-- наполнение таблицы student
+insert into student ('Vasya', 'Vasilkov');
+-- выбираю имя, фамилию студента
+select name, surname from student;
+
+/* Задание # 2.
+ * Описание задания (скопировать задание из файла с дз)
+*/
+update ...
+select ...
